2ae6e8fc67e64d10590aaf2e872dd050564caea5
ShahriarKh <31452340+ShahriarKh@users.noreply.github.com>
2022-12-09 11:22:30 +0330

    add search functionality

---
+++ components/SearchBox.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import css from "./SearchBox.module.scss";
+import Fuse from "fuse.js";
+
+export default function SearchBox({ setAvailabeEmojis, emojis }) {
+    const fuse = new Fuse(emojis, {
+        threshold: 0.1,
+        keys: ["label", "tags", "emoji", "hexcode", "emoticon"],
+    });
+
+    const [query, setQuery] = useState("");
+
+    function handleSearch(query) {
+        if (query == "") {
+            setAvailabeEmojis(emojis);
+            return;
+        }
+        let results = fuse.search(query);
+        let flatResults = Object.keys(results).reduce(function (r, k) {
+            return r.concat(results[k].item);
+        }, []);
+        setAvailabeEmojis(flatResults);
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => handleSearch(query), 200);
+        return () => clearTimeout(timeout);
+    }, [query]);
+
+    return (
+        <div>
+            <input onChange={(e) => setQuery(e.target.value)} />
+        </div>
+    );
+}
